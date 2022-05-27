5d601dd271fe90fc03e8433f73ead9931e1338e6
MorozDS <morozddima@gmail.com>
2022-05-27 22:54:31 +0300

    start work

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Profile from "components/Profile/profile"
+import user from './components/Profile/user.json'
+
+export default function App() {
+    return <div>
+        <Profile
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+  />
+    </div>
+}
