1bf7cb8aed029ea97dda815478b677a30d7e4f23
MorozDS <morozddima@gmail.com>
2022-06-01 23:06:19 +0300

    добавляю стили

--- src/components/Statistic/statistic.js
+++ src/components/Statistic/statistic.js
@@ -1,18 +1,19 @@
 import PropTypes from 'prop-types';
 import data from "./data.json"
+import s from "./statistic.module.css"
 
 
 export default function Statistic() {
   
-    return (<section className="statistics">
-  <h2 className="title">Upload stats</h2>
+  return (<section className={s.statistics}>
+      <h2 className={s.title}>Upload stats</h2>
       
-      <ul className="stat-list">
+      <ul className={s.statlist}>
         
         {data.map(({ id, label, percentage }) => 
-         (<li className="item" key={id}>
-          <span className="label">{label}</span>
-          <span className="percentage">{percentage}</span>
+        (<li className={s.item} key={id}>
+          <span className={s.label}>{label}</span>
+          <span className={s.percentage}>{percentage}%</span>
     </li>)
 )}
         
